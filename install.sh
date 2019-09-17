e225e0f2bf901fececfba79ce51e7af4ea726c4c
antortjim <ntoniohu@gmail.com>
2019-09-17 19:55:27 +0200

    make sure python3 is used

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
 
-INSTALLED=$(pip list | grep pypylon | wc -l)
+INSTALLED=$(python3 -m pip list | grep pypylon | wc -l)
 if [ $INSTALLED -eq 0 ]
 then
   rm pypylon*.whl
-  https://github.com/basler/pypylon/releases/download/1.4.0/pypylon-1.4.0-cp37-cp37m-linux_x86_64.whl
-  pip install pypylon*whl
+  wget https://github.com/basler/pypylon/releases/download/1.4.0/pypylon-1.4.0-cp37-cp37m-linux_x86_64.whl
+  python3 -m pip install pypylon*whl
 fi
 
-INSTALLED=$(pip list | grep LeMDT | wc -l)
+INSTALLED=$(python3 -m pip list | grep LeMDT | wc -l)
 if [ $INSTALLED -eq 1 ]
 then
  echo 'Found previous installation of LeMDT. Uninstalling now'
- pip uninstall -y LeMDT
+ python3 -m pip uninstall -y LeMDT
 fi
 
-pip install LeMDT*.whl
+python3 -m pip install LeMDT*.whl
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PYTHON_EXECUTABLE=`which python`
-PYTHON_DIR=$(dirname `which python`)
+PYTHON_EXECUTABLE=`which python3`
+PYTHON_DIR=$(dirname `which python3`)
 cd $DIR
 printf '#!/usr/bin/env xdg-open
 [Desktop Entry]
