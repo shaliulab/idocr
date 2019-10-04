8ab4681c4f698a405e42bc20826ea82c8818415f
antortjim <ntoniohu@gmail.com>
2019-10-04 14:13:10 +0200

    add script to make it easier to manually control arduino

---
+++ scripts/control.sh
@@ -0,0 +1,1 @@
+bash -c 'LD_LIBRARY_PATH=/home/vibflysleep/anaconda3/envs/py37/bin/../lib/ /home/vibflysleep/anaconda3/envs/py37/bin/python LeMDT/utils/control.py --value 1 --port /dev/ttyACM0 --pins 11'
