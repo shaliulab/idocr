4bcea7a839fe0cd7849196511361fb6c8e3e97fb
Antonio <ntoniohu@gmail.com>
2020-06-04 23:07:05 +0000

    Update configuration

--- py_src/learnmem/configuration.py
+++ py_src/learnmem/configuration.py
@@ -1,8 +1,11 @@
-import os, json, datetime
+import datetime
+import json
+import os
 import logging
 
 import numpy as np
 
+logging.basicConfig(level=logging.INFO)
 
 class LearnMemConfiguration(object):
     '''
@@ -23,16 +26,21 @@ class LearnMemConfiguration(object):
         'folders' : {
                         'results' : {'path' : '/learnmem_data/results', 'description' : 'Where data will be saved by the saver class.'},
                         'temporary' : {'path' : '/ethoscope_data/results', 'description' : 'A temporary location for downloading data.'},
-                        'programs': {'path': '/1TB/Cloud/Lab/Gitlab/learnmem/py_src/programs', 'description' : 'Where csv files containing hardware programs are stored.'}
+                        'paradigms': {'path': '/1TB/Cloud/Lab/Gitlab/learnmem/py_src/paradigms', 'description' : 'Where csv files containing hardware programs are stored.'}
         },
 
-        'users' :   {
-                        'admin' : {
-                            'id' : 1, 'name' : 'admin', 'fullname' : '', 'PIN' : 9999, 'email' : '',
-                            'telephone' : '', 'group': '', 'active' : False, 'isAdmin' : True,
-                            'created' : datetime.datetime.now().timestamp()
-                        }
-                    },
+        'users' : {
+                      'admin' : { # pylint: disable=bad-continuation
+                          'id' : 1, 'name' : 'admin', 'fullname' : '', 'PIN' : 9999, 'email' : '',
+                          'telephone' : '', 'group': '', 'active' : False, 'isAdmin' : True,
+                          'created' : datetime.datetime.now().timestamp()
+                      }
+        },
+
+        'roi_builder': {
+            'args': (),
+            'kwargs': {}
+        },
 
         'io': {
             'result_writer': {
@@ -47,15 +55,18 @@ class LearnMemConfiguration(object):
                 'args': (),
                 'kwargs': {
                     'framerate': 10, 'exposure_time': 5000,
+                    'resolution': (960, 720),
                     'drop_each': 1, 'max_duration': np.inf, 'wrap': True
                 }
-                # 'experimenter': 'Sayed', 'crop': 1, 'N': 10, 'fail': 'failed_arenas/'
             }
         },
 
         'controller': {
             'board_name': 'ArduinoDummy',
-            'mapping_path': '/1TB/Cloud/Lab/Gitlab/learnmem/py_src/mappings/mega.csv'
+            'mapping_path': '/1TB/Cloud/Lab/Gitlab/learnmem/py_src/mappings/mega.csv',
+            'paradigm_path': 'unittest_long.csv',
+            'adaptation_time': 600,
+            'arduino_port': "/dev/ttyACM0"
         },
 
         'drawer': {
@@ -83,7 +94,6 @@ class LearnMemConfiguration(object):
             "odor_B":  'B',
             "save_results_answer":  'Yes',
             "record_start":  0,
-            "adaptation_time_minutes":  0,
             "decision_zone_mm": 10,
             "min_exits_required": 5,
             "max_time_minutes": 60,
@@ -103,14 +113,14 @@ class LearnMemConfiguration(object):
 
     @property
     def file_exists(self):
-        '''
-        '''
+
         return os.path.exists(self._config_file)
 
     def save(self):
-        '''
+        """
         Save settings to default json file
-        '''
+        """
+
         try:
             with open(self._config_file, 'w') as json_data_file:
                 json.dump(self._settings, json_data_file)
@@ -118,7 +128,7 @@ class LearnMemConfiguration(object):
             logging.info('Saved idoc configuration file to %s', self._config_file)
 
         except:
-            raise ValueError ('Problem writing to file % s' % self._config_file)
+            raise ValueError('Problem writing to file % s' % self._config_file)
 
     def load(self):
         '''
@@ -126,12 +136,13 @@ class LearnMemConfiguration(object):
         If file does not exist, creates default settings
         '''
 
-        if not self.file_exists : self.save()
+        if not self.file_exists:
+            self.save()
 
         else:
             try:
                 with open(self._config_file, 'r') as json_data_file:
-                    self._settings.update ( json.load(json_data_file) )
+                    self._settings.update(json.load(json_data_file))
             except:
                 raise ValueError("File %s is not a valid configuration file" % self._config_file)
 
