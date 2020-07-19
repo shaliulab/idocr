d858dec9cbeeb60ccf6125161ee6dd9fdd83b22e
Antonio <ntoniohu@gmail.com>
2020-07-19 16:46:23 +0000

    Remove old code and remove one level of depth

--- R_src/idocr/inst/main.R
+++ R_src/idocr/inst/main.R
@@ -5,3 +5,9 @@ p2 <- main(experiment_folder = '/1TB/Cloud/Data/idoc_data/results/FLYSLEEPLAB_SE
 p3 <- main(experiment_folder = "/1TB/Cloud/Data/idoc_data/results/FLYSLEEPLAB_SETUP/2020-06-12_15-00-24", border = 10, min_exits_required = 3)
 cowplot::plot_grid(p1, p2, nrow = 2)
 p4 <- main(experiment_folder = "/1TB/Cloud/Data/idoc_data/results/FLYSLEEPLAB_SETUP/2020-06-12_19-40-37/", border = 10, min_exits_required = 3, hardware = c('ODOR_A_LEFT', 'ODOR_A_RIGHT'))
+
+experiment_folder <- system.file(
+  "idoc_data/results//7eb8e224bdb944a68825986bc70de6b1/IDOC_001/2020-07-17_15-01-10/2020-07-17_15-01-10_7/",
+  package = "idocr"
+) 
+p5 <- main(experiment_folder = experiment_folder, border = 10, min_exits_required = 3, hardware = c('ODOR_A_LEFT', 'ODOR_A_RIGHT'))
