d07a96ed90f91e3a5985d4638570a8d13ae8ac8f
antortjim <ntoniohu@gmail.com>
2020-10-06 17:57:06 +0200

    Document main.R so it becomes in practice a CLI application to idocr

--- inst/main.R
+++ inst/main.R
@@ -1,34 +1,71 @@
 library(idocr)
-
 setwd('/1TB/Cloud/Data/idoc_data/results/7eb8e224bdb944a68825986bc70de6b1/IDOC_001')
-experiment_folder <- "2020-10-05_13-05-51"
-experiment_folder <- "~/R/x86_64-pc-linux-gnu-library/3.6/idocr/2020-10-05_13-05-51"
-src_file <- rstudioapi::getActiveDocumentContext()$path
 
-# change what treatment A and B are
+##############################################
+# CHANGE CODE BELOW THIS LINE
+##############################################
+
+#### Probably you want to change this
+
+# Supply an experiment contained in the working directory
+# Press TAB to autocomplete
+experiment_folder <- "2020-10-05_13-05-51"
+
+
+# Change what treatment A and B are
 # to fit it to your needs
 treatment_A <- "OCT"
-treatment_B <- "MCH"
+treatment_B <- "MCH+ES"
+
+# leave this empty or fill something relevant for the experiment
+# it will appear in the subtitle of the plot
+# (below the big title)
+description <- "Enter a description here"
+
+# Minimal number of exits required to consider the data
+# of one animal significant and then compute a index from it
+min_exits_required <- 3
+
+# Apply a time offset to the treatment time series
+# to account for the time it takes for odour to
+# arrive to the chambers
+# Units in seconds
+delay <- 0
+
+
+#### Probably you dont want to change this
+#### Please change these numbers only if you know what you are doing
+
+# Define a distance from the center of the chamber
+# to the decision zone
+# Units in mm
+border_mm <- 5
+
+# Behavioral masking
+# Ignore exits happening this amount of seconds
+# after the previous exit
+# to avoid counting the same exit 
+# as two exits happening within ridiculously little time  
+
+##################################################
+# CAUTION!! DONT CHANGE ANY CODE BELOW THIS LINE
+##################################################
+
 treatments <- c(
   TREATMENT_A = treatment_A,
   TREATMENT_B = treatment_B
 )
 
+src_file <- rstudioapi::getActiveDocumentContext()$path
 
-# leave it empty or fill something relevant for the experiment
-# it will appear in the subtitle of the plot
-# (below the big title)
-description <- ""
-
-p1 <- idocr(experiment_folder = experiment_folder, #select the experiment folder
+p1 <- idocr(experiment_folder = experiment_folder,
             treatments = treatments,
-            border = 5, # mm from the center of the chamber to the decision zone,
-            min_exits_required = 3,
+            border_mm = border_mm,
+            min_exits_required = min_exits_required,
             src_file = src_file,
             subtitle = description,
-            delay = 0, # seconds offset,
-            mask_duration = 0.5 # mask behavior this amount of seconds after an exit,
-            # to avoid counting the same exit twice as two exits that happen within ridiculously little time  
+            delay = delay,
+            mask_duration = mask_duration
             )
 
 export_summary(experiment_folder = experiment_folder)
