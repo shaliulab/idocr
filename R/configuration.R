20e07c8fa2e10eab51dd598edacc640af11ef42c
antortjim <ntoniohu@gmail.com>
2022-05-15 16:55:54 +0200

    Add messaging in configuration

--- R/configuration.R
+++ R/configuration.R
@@ -1,10 +1,14 @@
 CONFIG_FILE <- "analysis_params.yaml"
 DEFAULT_CONFIG <- list(pixels_to_mm_ratio = 2.3)
+#' @importFrom yaml read_yaml
+#' @import glue
 read_config <- function() {
   
   if (file.exists(CONFIG_FILE)) {
+      message(glue::glue("Reading configuration from {CONFIG_FILE}"))
       config <- yaml::read_yaml(CONFIG_FILE)
   } else {
+    message(glue::glue("{CONFIG_FILE} not found. Using default config"))
      config <- DEFAULT_CONFIG
   }
   return(config)
