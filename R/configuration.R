aef233cce8d818a746bbab06c497da5bd11d5d8e
antortjim <ntoniohu@gmail.com>
2023-09-12 20:02:17 +0200

    Update config

--- R/configuration.R
+++ R/configuration.R
@@ -1,10 +1,9 @@
 CONFIG_FILE <- "analysis_params.yaml"
-DEFAULT_CONFIG <- list(pixels_to_mm_ratio = 2.3)
+DEFAULT_CONFIG <- list(pixel_to_mm_ratio = 2.3)
 #' @importFrom yaml read_yaml
 #' @import glue
 #' @export
 read_config <- function() {
-  
   if (file.exists(CONFIG_FILE)) {
       message(glue::glue("Reading configuration from {CONFIG_FILE}"))
       config <- yaml::read_yaml(CONFIG_FILE)
